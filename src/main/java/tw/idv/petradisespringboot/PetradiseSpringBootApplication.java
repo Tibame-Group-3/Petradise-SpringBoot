4dce87dd823e65e0210e9c7b3e90ddb7aae38fb2
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-06-02 17:34:22 +0800

    initialize project

---
+++ src/main/java/tw/idv/petradisespringboot/PetradiseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.petradisespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetradiseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetradiseSpringBootApplication.class, args);
+	}
+
+}
