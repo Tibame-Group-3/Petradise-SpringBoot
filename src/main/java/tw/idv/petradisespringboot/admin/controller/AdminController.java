fcb288f52e553f754f2019e2e9ea7fc4198c7967
siajs <ksh70070@gmail.com>
2023-06-13 21:08:45 +0800

    test commit from IntelliJ

--- src/main/java/tw/idv/petradisespringboot/admin/controller/AdminController.java
+++ src/main/java/tw/idv/petradisespringboot/admin/controller/AdminController.java
@@ -1,7 +1,11 @@
 package tw.idv.petradisespringboot.admin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import tw.idv.petradisespringboot.admin.service.AdminService;
+import tw.idv.petradisespringboot.admin.vo.Admin;
+
 @RestController
 public class AdminController {
 
@@ -9,8 +13,10 @@ public class AdminController {
     public AdminController(AdminService service) {
         this.service = service;
     }
-
-
+    @GetMapping
+    Admin findById(@PathVariable Integer id){
+        return service.findById(id);
+    }
 
 
 
