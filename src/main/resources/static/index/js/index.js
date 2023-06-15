712ae002af40bac89f6a4e664fc1bca0c5ed9a92
HsuRamone <hsuramone@gmail.com>
2023-06-15 10:34:01 +0800

    feat: add signup page

--- src/main/resources/static/index/js/index.js
+++ src/main/resources/static/index/js/index.js
@@ -1,9 +1,24 @@
 (() => {
     $('.navigate-member-center')
         .on('click', onClickMemberCenter);
+    $('.navigate-signin')
+        .on('click', onClickSignin);
 })();
 
 function onClickMemberCenter(e) {
     e.preventDefault();
-    location.href = "/member/sign_up.html";
+    navigateToSignup();
+}
+
+function onClickSignin(e) {
+    e.preventDefault();
+    navigateToSignin();
+}
+
+function navigateToSignup() {
+    location.href = "/member/signup.html";
+}
+
+function navigateToSignin() {
+    location.href = "/member/signin.html"
 }
