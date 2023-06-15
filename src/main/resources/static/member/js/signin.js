69f33b7fa6c24070d7a318fd0251f02cd6a2309d
HsuRamone <hsuramone@gmail.com>
2023-06-15 11:37:03 +0800

    feat: sign in temp

--- src/main/resources/static/member/js/signin.js
+++ src/main/resources/static/member/js/signin.js
@@ -2,12 +2,30 @@
 
 
 $(document).ready(function () {
-
-
-
+    $('#btn_signin')
+        .on('click', onClickSignin);
 });
 
 
 function onClickSignin() {
+    validateForm();
+}
 
+function validateForm() {
+    const account = $('#account').val();
+    const password = $('#password').val();
+
+    if (!isAccountFormatValid(account)) {
+        showAlert("帳號輸入錯誤，請再次檢查");
+    }
+}
+
+function isAccountFormatValid(account) {
+
+}
+
+function showAlert(message) {
+    $('.alert')
+        .text(message)
+        .removeClass('d-none');
 }
