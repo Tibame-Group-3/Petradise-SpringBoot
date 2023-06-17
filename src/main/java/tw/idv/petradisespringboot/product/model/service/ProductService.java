23eec7073e9cce71e90145b027f8d2923213a6c3
Morgan <cherry61228@gmail.com>
2023-06-17 12:56:27 +0800

    product - update mvc

---
+++ src/main/java/tw/idv/petradisespringboot/product/model/service/ProductService.java
@@ -0,0 +1,13 @@
+package tw.idv.petradisespringboot.product.model.service;
+
+import tw.idv.petradisespringboot.news.model.vo.News;
+import tw.idv.petradisespringboot.product.model.vo.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    Product insert(Product newProduct);
+    Product update(Product product);
+    Product getOneById(Integer productId);
+    List<Product> getAll();
+}
