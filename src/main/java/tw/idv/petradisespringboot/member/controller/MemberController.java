d8891bda25279264c485552125bfbaf100112579
HsuRamone <hsuramone@gmail.com>
2023-06-17 14:55:25 +0800

    feat: add taiwan district list

--- src/main/java/tw/idv/petradisespringboot/member/controller/MemberController.java
+++ src/main/java/tw/idv/petradisespringboot/member/controller/MemberController.java
@@ -1,12 +1,18 @@
 package tw.idv.petradisespringboot.member.controller;
 
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tw.idv.petradisespringboot.member.service.MemberService;
+import tw.idv.petradisespringboot.member.vo.AddressInfo;
 import tw.idv.petradisespringboot.member.vo.Member;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
@@ -14,8 +20,11 @@ class MemberController {
 
     private final MemberService service;
 
-    public MemberController(MemberService service) {
+    private final ResourceLoader resourceLoader;
+
+    public MemberController(MemberService service, ResourceLoader resourceLoader) {
         this.service = service;
+        this.resourceLoader = resourceLoader;
     }
 
     @GetMapping("/all")
@@ -54,6 +63,20 @@ class MemberController {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new MemberNotFoundException("帳號或密碼有誤"));
+    }
+
+    @GetMapping("/districts")
+    ResponseEntity<?> districts() throws Exception {
+        Resource resource = loadDistricts();
+        ObjectMapper mapper = new ObjectMapper();
+        JavaType type = mapper.getTypeFactory().constructParametricType(List.class, AddressInfo.class);
+        List<AddressInfo> infos = mapper.readValue(resource.getInputStream(), type);
+        System.out.println(infos);
+        return ResponseEntity.ok(infos);
+    }
+
+    private Resource loadDistricts(){
+        return resourceLoader.getResource("classpath:json/address_info.json");
     }
 }
 
