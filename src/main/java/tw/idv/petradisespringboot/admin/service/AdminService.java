3490fd3b608016fde6d5de61aa5117502c883900
yuhsiu <hermosaliao0928@gmail.com>
2023-06-22 00:19:40 +0800

    暫存

--- src/main/java/tw/idv/petradisespringboot/admin/service/AdminService.java
+++ src/main/java/tw/idv/petradisespringboot/admin/service/AdminService.java
@@ -1,22 +1,22 @@
-package tw.idv.petradisespringboot.admin.service;
-
-import tw.idv.petradisespringboot.admin.vo.Admin;
-
-import java.util.List;
-
-public interface AdminService {
-
-    Admin add(Admin admin);
-    Admin modify(Integer id, Admin updatedAdmin);
-    Admin findById(Integer id);
-    List<Admin> searchAdminsByName(String keyword);
-    List<Admin> findAdminsByTitle(char title);
-    List<Admin> findAdminsByStatus(char status);
-    List<Admin> getAdminsByIdOrderByTitle();
-    List<Admin> getAdminsByIdOrderByStatusDesc();
-    Admin changeAdminTitle(Integer id, char newTitle);
-    Admin changeAdminStatus(Integer id, char newStatus);
-
-
-
-}
+//package tw.idv.petradisespringboot.admin.service;
+//
+//import tw.idv.petradisespringboot.admin.vo.Admin;
+//
+//import java.util.List;
+//
+//public interface AdminService {
+//
+//    Admin add(Admin admin);
+//    Admin modify(Integer id, Admin updatedAdmin);
+//    Admin findById(Integer id);
+//    List<Admin> searchAdminsByName(String keyword);
+//    List<Admin> findAdminsByTitle(char title);
+//    List<Admin> findAdminsByStatus(char status);
+//    List<Admin> getAdminsByIdOrderByTitle();
+//    List<Admin> getAdminsByIdOrderByStatusDesc();
+//    Admin changeAdminTitle(Integer id, char newTitle);
+//    Admin changeAdminStatus(Integer id, char newStatus);
+//
+//
+//
+//}
