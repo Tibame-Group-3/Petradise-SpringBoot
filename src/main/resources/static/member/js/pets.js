ccb4093d110c8da28fbf9e302ba706a78952ee12
HsuRamone <hsuramone@gmail.com>
2023-06-21 10:44:10 +0800

    feat: 成功抓到會員寵物

--- src/main/resources/static/member/js/pets.js
+++ src/main/resources/static/member/js/pets.js
@@ -1,35 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     fetchPets();
-    displayPets();
     addNewPetButtonListener();
 });
 
 function fetchPets() {
     fetch(`/pets/memberId=${getMemberId()}`)
         .then(response => response.json())
-        .then(console.log)
+        .then(displayPets)
 }
 
-function displayPets() {
-    // Replace this array with your actual data
-    const pets = [
-        {
-            name: 'Charlie',
-            image: 'https://example.com/charlie.jpg',
-        },
-        {
-            name: 'Max',
-            image: 'https://example.com/max.jpg',
-        }
-        // Add more pets here...
-    ];
+function displayPets(pets) {
 
     const petsRow = document.getElementById('pets-row');
     pets.forEach(pet => {
+        let image = 'https://placehold.co/268x180';
+        const petPics = pet.petPics;
+        if (petPics !== undefined && petPics.length > 0) {
+            image = ` data:image/jpeg;charset=utf-8;base64, ${petPics[0].pic}`;
+        }
         const petCard = `
             <div class="col-md-4">
                 <div class="card mb-4">
-                    <img src="${pet.image}" class="card-img-top" alt="${pet.name}">
+                    <img src="${image}" class="card-img-top" alt="${pet.name}">
                     <div class="card-body">
                         <h5 class="card-title">${pet.name}</h5>
                     </div>
