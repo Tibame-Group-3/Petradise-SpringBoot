5fb9f07ce9ee09d7f205c808f50c0f689d7856c9
Morgan <cherry61228@gmail.com>
2023-06-21 16:04:12 +0800

    feat: chage datatable views

--- src/main/java/tw/idv/petradisespringboot/news/model/vo/News.java
+++ src/main/java/tw/idv/petradisespringboot/news/model/vo/News.java
@@ -21,22 +21,18 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Table(name = "news_list")
+@Table(name = "news")
 public class News {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "news_id")
     Integer newsId;
-    @Column(name = "admin_id")
-    Integer adminId;
     @Column(name = "news_title")
     String newsTitle;
     @Column(name = "news_content")
     String newsContent;
     @Column(name = "news_date")
     Date newsDate;
-    @Column(name = "news_photo", insertable = false)
-    byte[] newsPhoto;
 
 }
