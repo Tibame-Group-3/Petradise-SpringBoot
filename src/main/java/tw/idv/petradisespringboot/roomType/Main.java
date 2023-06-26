bded854dcd303cc301173b2dfb7d46fbcb6bd57f
peipei30 <133873451+peipei30@users.noreply.github.com>
2023-06-26 20:07:30 +0800

    hotel-testmain

--- src/main/java/tw/idv/petradisespringboot/roomType/Main.java
+++ src/main/java/tw/idv/petradisespringboot/roomType/Main.java
@@ -3,6 +3,7 @@ package tw.idv.petradisespringboot.roomType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import tw.idv.petradisespringboot.roomType.dto.SingleHotelDTO;
 import tw.idv.petradisespringboot.roomType.repo.RoomTypeRepository;
 import tw.idv.petradisespringboot.roomType.service.impl.RoomTypeServiceImpl;
 import tw.idv.petradisespringboot.roomType.vo.RoomType;
@@ -18,11 +19,7 @@ public class Main implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<RoomType> roomTypes = roomTypeRepository.searchHotel("桃園市", "狗", "小型", "喵響小屋");
-        for (RoomType roomType :
-                roomTypes
-        ) {
-            System.out.println(roomType);
-        }
+        SingleHotelDTO singleHotel = roomTypeService.getSingleHotel(5, "貓", '0');
+        System.out.println(singleHotel);
     }
 }
