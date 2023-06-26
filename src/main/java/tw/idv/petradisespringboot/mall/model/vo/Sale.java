0aeaf475f9f705a197d14bffc1edb9d633c5524a
David <m7689150@hotmail.com>
2023-06-26 20:51:43 +0800

    update

--- src/main/java/tw/idv/petradisespringboot/mall/model/vo/Sale.java
+++ src/main/java/tw/idv/petradisespringboot/mall/model/vo/Sale.java
@@ -2,12 +2,12 @@ package tw.idv.petradisespringboot.mall.model.vo;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,29 +18,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name = "sale")
 public class Sale {
-	
-	@Id
-	@Column(name = "sale_pro_id")
-	private Integer saleProId;
 
-	@Column(name = "pd_type")
-	private Integer pdType;
+	@EmbeddedId
+	private SaleCompositePK id;
 	
 	@Column(name = "sale_discount")
 	private Double saleDiscount;
 	
-	@JsonBackReference
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="sale_pro_id", referencedColumnName = "sale_pro_id",insertable = false, updatable=false)
-	private SaleProject saleProject;
+    @ManyToOne
+    @JoinColumn(name = "pd_id", insertable = false, updatable=false)
+    private Product product;
 	
-	@Override
-	public String toString() {
-		return "Sale {" +
-				"saleProId=" + saleProId +
-				"pdType=" + pdType +
-				"saleDiscount=" + saleDiscount +
-				'}';
-	}
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "sale_pro_id", insertable = false, updatable=false)
+	private SaleProject saleProject;
 	
 }
--- src/main/java/tw/idv/petradisespringboot/mall/model/vo/SaleProject.java
+++ src/main/java/tw/idv/petradisespringboot/mall/model/vo/SaleProject.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,7 +14,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,7 +23,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 @Table(name = "sale_project")
 public class SaleProject {
 
@@ -36,25 +35,14 @@ public class SaleProject {
 	private String saleProName;
 
 	@Column(name = "sale_pro_start")
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Timestamp saleProStart;
 
 	@Column(name = "sale_pro_end")
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
 	private Timestamp saleProEnd;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy = "saleProId", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "saleProject", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<Sale> sale;
-	
-	@Override
-	public String toString() {
-		return "SaleProject {" + 
-				"saleProId=" + saleProId + 
-				"saleProName=" + saleProName + 
-				"saleProStart=" + saleProStart + 
-				"saleProEnd=" + saleProEnd + 
-				'}';
-	}
 
 }
