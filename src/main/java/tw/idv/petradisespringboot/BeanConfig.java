728e50d2ba7d2280c2606527a4d5e996f9d95169
HsuRamone <hsuramone@gmail.com>
2023-06-28 19:49:53 +0800

    feat: successfully sent test email

--- src/main/java/tw/idv/petradisespringboot/BeanConfig.java
+++ src/main/java/tw/idv/petradisespringboot/BeanConfig.java
@@ -1,7 +1,12 @@
 package tw.idv.petradisespringboot;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration // indicates this is a configuration class.
 public class BeanConfig {
@@ -10,4 +15,25 @@ public class BeanConfig {
     public ModelMapper metodoQueCriaUmModelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("tibamegp3@gmail.com");
+        mailSender.setPassword("omqrofpcgzxeypaw");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+
+
 }
---
+++ src/main/java/tw/idv/petradisespringboot/email/EmailServiceImpl.java
@@ -0,0 +1,25 @@
+package tw.idv.petradisespringboot.email;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender sender;
+
+    public EmailServiceImpl(JavaMailSender sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("tibamegp3@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        sender.send(message);
+    }
+}
