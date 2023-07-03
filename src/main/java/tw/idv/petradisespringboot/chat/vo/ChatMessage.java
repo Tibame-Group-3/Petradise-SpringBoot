ac54b58c6873e9713b6181ca6b37ff2864a5c7b8
xingming531 <happy850531@gmail.com>
2023-07-03 15:02:13 +0800

    feat: update chat repo&vo & html & css &js

--- src/main/java/tw/idv/petradisespringboot/chat/vo/ChatMessage.java
+++ src/main/java/tw/idv/petradisespringboot/chat/vo/ChatMessage.java
@@ -9,53 +9,63 @@ import org.springframework.data.redis.core.RedisHash;
 @RedisHash("chatMessage")
 public class ChatMessage {
 
-	
 	@Id
 	private String id;
 	private ChatType type;
-    /** 訊息發送者的名稱 */
-    private String sender;
-    /** 訊息內容 */
-    private String content;
-    private Instant timestamp;
+	/** 訊息發送者的名稱 */
+	private String sender;
+	/** 訊息內容 */
+	private String content;
+	private Instant timestamp;
+	private String hotelId;
 
-	 /**
-     * 訊息種類Enum
-     */
-	   public enum ChatType {
-	        CHAT,
-	     
-	        LEAVE
-	    }
-	   public void setChatType(ChatType type) {
-		   this.type = type;
-		   }
+	/**
+	 * 訊息種類Enum
+	 */
+	public enum ChatType {
+		CHAT,
 
-		   public ChatType getChatType() {
-		   return this.type;
-		   }
+		LEAVE
+	}
 
-		   public void setSender(String sender) {
-		   this.sender = sender;
-		   }
+	public void setChatType(ChatType type) {
+		this.type = type;
+	}
 
-		   public String getSender() {
-		   return this.sender;
-		   }
+	public ChatType getChatType() {
+		return this.type;
+	}
 
-		   public void setContent(String content) {
-		   this.content = content;
-		   }
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
 
-		   public String getContent() {
-		   return this.content;
-		   }
+	public String getSender() {
+		return this.sender;
+	}
 
-		public Instant getTimestamp() {
-			return timestamp;
-		}
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-		public void setTimestamp(Instant timestamp) {
-			this.timestamp = timestamp;
-		}
+	public String getContent() {
+		return this.content;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(String hotelId) {
+		this.hotelId = hotelId;
+	}
+
 }
