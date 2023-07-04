8138a3bac18eb5113697932deb4388cd57ccd3ba
20_ <uuq55426@gmail.com>
2023-07-04 13:32:32 +0800

    錯誤驗證 及串接其餘功能...

--- src/main/java/tw/idv/petradisespringboot/animalcorporation/controller/AnimalCorporationController.java
+++ src/main/java/tw/idv/petradisespringboot/animalcorporation/controller/AnimalCorporationController.java
@@ -52,8 +52,24 @@ public class AnimalCorporationController {
 	}
 	
 	@PostMapping("/apply")
-	AnimalCorporation apply(@RequestBody AnimalCorporation animalCorporation) {
-		return service.add(animalCorporation);
+	ResponseEntity<?> apply(@RequestBody AnimalCorporation animalCorporation) {
+		
+		if (animalCorporation == null) {
+			ResponseEntity.notFound().build();
+		}
+		if (ResponseEntity.ok() != null) {
+			emailService.sendEmail(animalCorporation.getContactEmail(), 
+					"您的Petradise帳戶正在審核中", 
+					"Hi," + animalCorporation.getContactName() + "\n"
+					+ "已收到您的帳戶申請" + "\n"
+					+ "請靜待回覆" + "\n"
+					+ "感謝您的申請\n"
+					+ "Petradise團隊敬上");
+		}
+		
+		service.add(animalCorporation);
+	
+		return ResponseEntity.ok("OK");
 	}
 	
 	@ResponseBody
@@ -63,7 +79,7 @@ public class AnimalCorporationController {
 	}
 	
 	@PutMapping("/editCorpAccess/id={id}")
-	ResponseEntity<AnimalCorporation> editCorpAccess(@PathVariable Integer id){
+	ResponseEntity<?> editCorpAccess(@PathVariable Integer id){
 		AnimalCorporation corporation = service.findByID(id);
 		
 		if (corporation == null) {
@@ -98,14 +114,16 @@ public class AnimalCorporationController {
 							+ "您已成功通過帳號申請成為合作業主，\n"
 							+ "您的帳號是" +  animalCorporation.getCorpAccount() + "\n"
 							+ "您的密碼是" + animalCorporation.getCorpPassword() + "\n"
-							+ "請至 http://xxxxxxxx/ 登入服務");
+							+ "請至 http://xxxxxxxx/ 登入服務\n"
+							+ "Petradise團隊敬上");
 		} else if (appliedStatus == '1'){
 			emailService.sendEmail(animalCorporation.getContactEmail(), 
 					"您的Petradise帳戶申請結果", 
 					"Hi," + animalCorporation.getContactName() + "\n"
 					+ "您的申請結果為未通過\n"
 					+ "感謝您的申請\n"
-					+ "Petradise期待繼續替您服務");
+					+ "Petradise期待繼續替您服務\n"
+					+ "Petradise團隊敬上");
 		}
 		animalCorporation.setAppliedStatus(appliedStatus);
 		service.update(animalCorporation);
