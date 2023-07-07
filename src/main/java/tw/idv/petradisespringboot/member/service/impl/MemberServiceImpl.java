2135256b870670a2ca6ce27d7aa32f6bcdcb7fcf
HsuRamone <hsuramone@gmail.com>
2023-07-07 14:03:26 +0800

    feat: 修正會員帳號被停權後還是可以登入的錯誤

--- src/main/java/tw/idv/petradisespringboot/member/service/impl/MemberServiceImpl.java
+++ src/main/java/tw/idv/petradisespringboot/member/service/impl/MemberServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import tw.idv.petradisespringboot.email.EmailService;
+import tw.idv.petradisespringboot.hotel_owner.exceptions.AccountNotFoundException;
 import tw.idv.petradisespringboot.member.dto.MemberDTO;
 import tw.idv.petradisespringboot.member.dto.SignUpDTO;
 import tw.idv.petradisespringboot.member.dto.UpdateDTO;
@@ -60,6 +61,9 @@ public class MemberServiceImpl implements MemberService {
                 .orElseThrow(() -> new LoginException("帳號或密碼錯誤"));
         if (!dto.getIsEmailVerified()) {
             throw new MemberNotVerifiedException("請先驗證電子郵件");
+        }
+        if (Objects.equals(dto.getAccess(), MemberAccess.INACTIVE)) {
+            throw new AccountNotFoundException("帳號已被停權");
         }
         return dto;
     }
