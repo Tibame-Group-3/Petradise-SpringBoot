041438154bbb5e916bc74b0e24c53cf2e9c06f44
20_ <uuq55426@gmail.com>
2023-07-09 18:22:54 +0800

    edit serviceimpl

--- src/main/java/tw/idv/petradisespringboot/lostpetarticle/repo/LostPetArticleRepository.java
+++ src/main/java/tw/idv/petradisespringboot/lostpetarticle/repo/LostPetArticleRepository.java
@@ -1,6 +1,7 @@
 package tw.idv.petradisespringboot.lostpetarticle.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
